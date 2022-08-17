9547b167a283b829e744e4731a2d4f8a39bcf69d
julian trejos <julian>
2022-08-16 21:28:53 -0500

    pagina para pantallas grandes terminada

--- script.js
+++ script.js
@@ -7,18 +7,32 @@ const inputCopiar = document.querySelector(".Copiar");
 
 function encriptar(){
     var mensaje = textareaMesaje.value;
+    var arreglo = new String(mensaje);
     var mensajeEncriptado = mensaje
     .replaceAll("e","enter")
     .replaceAll("i","imes")
     .replaceAll("o","ober")
     .replaceAll("a","ai")
-    .replaceAll("u","ufat");  
+    .replaceAll("u","ufat");
     textareaResultado.value = mensajeEncriptado;
+    for(var i=0;i<arreglo.length;i++){
+    
+        if(arreglo.length>0){
+            document.getElementById('img').style.display = 'none';
+            document.getElementById('aviso1').style.display = 'none';
+            document.getElementById('aviso2').style.display = 'none';
+            document.getElementById('boton3').style.display = 'block';
+            document.getElementById('mensajeEncontrado').style.display = 'block';
+        }  
+        else{
+          
+         }
+    }     
 }
-
 
 function Desencriptar(){
     var mensajeEncriptado = textareaMesaje.value;
+    var arreglo = new String(mensajeEncriptado);
     var mensaje = mensajeEncriptado
     .replaceAll("ai","a")
     .replaceAll("enter","e")
@@ -26,14 +40,41 @@ function Desencriptar(){
     .replaceAll("ober","o")
     .replaceAll("ufat","u");  
     textareaResultado.value = mensaje;
+    for(var i=0;i<arreglo.length;i++){
+    
+        if(arreglo.length>0){
+            document.getElementById('img').style.display = 'none';
+            document.getElementById('aviso1').style.display = 'none';
+            document.getElementById('aviso2').style.display = 'none';
+            document.getElementById('boton3').style.display = 'block';
+            document.getElementById('mensajeEncontrado').style.display = 'block';
+        }  
+        else{
+          
+         }
+    }   
 }
- 
+ function borrar(){
+    textareaResultado.value="";
+    textareaMesaje.focus();
+    textareaMesaje.value="";
+    document.getElementById('img').style.display = 'block';
+    document.getElementById('aviso1').style.display = 'block';
+    document.getElementById('aviso2').style.display = 'block';
+    document.getElementById('boton3').style.display = 'none';
+    document.getElementById('mensajeEncontrado').style.display = 'none';
+ }
 
 function copiar(){
-    var mensajeEncriptado = textareaResultado.value;
+    var mensajeEncriptado = textareaResultado.value
     navigator.clipboard.writeText(mensajeEncriptado);
-    
+    document.getElementById('img').style.display = 'block';
+    document.getElementById('aviso1').style.display = 'block';
+    document.getElementById('aviso2').style.display = 'block';
+    document.getElementById('boton3').style.display = 'none';
+    document.getElementById('mensajeEncontrado').style.display = 'none';
 }
 inputEncriptar.onclick = encriptar;
 inputDesEncriptar.onclick = Desencriptar;
 inputCopiar.onclick = copiar;
+textareaMesaje.onclick = borrar;
